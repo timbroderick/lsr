d7332c7df5340ca64b917a7c59294103705296a8
timbroderik <aabroder@gmail.com>
2020-01-25 10:36:01 -0600

    quick chapter 8

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 This is my repository for the work I'll while I go through [Learning statistics with R: A tutorial for psychology students and other beginners.](https://learningstatisticswithr.com/book/)
 
 Resources and more available [here](https://learningstatisticswithr.com/)
+
+Mostly this is for review, and prepping for [Introduction to Statistical Learning](https://github.com/timbroderick/learnstats)
---
+++ chapter_8_programming/chapter_8.R
@@ -0,0 +1,33 @@
+# set directory
+setwd("~/anaconda3/envs/lsr/chapter_7_programming")
+
+# just a few useful tidbits
+
+# load libraries
+library(tidyverse)
+options("scipen" = 10)
+
+# the while loop
+x <- 0
+while ( x <= 10 ) {
+  x <- x + 1
+  print(x)
+}
+
+
+# functions
+quadruple <- function(x) {
+  y <- x*4
+  return(y)
+}
+
+quadruple(10)
+
+# specifying a function with a default value
+pow <- function( x, y = 1) {
+  out <- x^y  # raise x to the power y
+  return( out )
+}
+
+pow(x=15) # leaves the default for y
+pow(15,10) # specifies x and y
